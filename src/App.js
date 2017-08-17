2cf42c7e10ae90f0c62c78e18ca61101cb83fe79
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2017-08-17 23:44:14 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+const Home = () => {
+  <div>
+    <h2>Home</h2>
+  </div>
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
